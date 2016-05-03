6aee27c48b74fe2c6733aa8e01113620e46efe0a
Shariq Mobin <Shariq.Mobin@gmail.com>
2016-05-03 12:26:15 -0700

    better looking

--- graph_L.py
+++ graph_L.py
@@ -5,9 +5,9 @@ import pdb
 
 Ls = sio.loadmat('mats/Ls.mat')['x'][:,0]
 plt.plot(Ls)
-plt.title("L_emp over time", fontdict={'fontsize':20})
-plt.xlabel("Iteration #", fontdict={'fontsize':18})
-plt.ylabel("L_emp", fontdict={'fontsize':18})
+plt.title("L_k over time", fontdict={'fontsize':20})
+plt.xlabel("Iteration # (k)", fontdict={'fontsize':18})
+plt.ylabel("L_k", fontdict={'fontsize':18})
 # plt.axis([0, 200, 0, 10])
 
 plt.show()
--- nn_graph.py
+++ nn_graph.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import scipy.io as sio
 import pdb
 
-sgd_loss  = sio.loadmat('sgd_back.mat')['x'][:,0]
-rms_loss  = sio.loadmat('rmsprop.mat')['x'][:,0]
-adam_loss = sio.loadmat('adam.mat')['x'][:,0]
+sgd_loss  = sio.loadmat('mats/sgd_back.mat')['x'][:,0]
+rms_loss  = sio.loadmat('mats/rmsprop.mat')['x'][:,0]
+adam_loss = sio.loadmat('mats/adam.mat')['x'][:,0]
 
-plt.plot(sgd_loss,  label='SGD-Back')
-plt.plot(rms_loss,  label='RMSProp Loss')
-plt.plot(adam_loss, label='Adam Loss')
+lw=6
+plt.plot(sgd_loss,  label='SGD-Back', linestyle='-', lw=lw)
+plt.plot(rms_loss,  label='RMSProp Loss', linestyle=':', lw=lw)
+plt.plot(adam_loss, label='Adam Loss', linestyle='--', lw=lw)
 
 plt.title("Convergence for various algorithms", fontdict={'fontsize':20})
-plt.xlabel("Iteration #", fontdict={'fontsize':18})
+plt.xlabel("Iteration # (k)", fontdict={'fontsize':18})
 plt.ylabel("Loss", fontdict={'fontsize':18})
 plt.axis([0, 200, 0, 10])
 
